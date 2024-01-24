de958402c6b89c2ce67f11ad7c30261893648b5f
choiyunjung88 <lucy023@sookmyung.ac.kr>
2024-01-24 17:30:51 +0900

    Day10 01/24

--- 3_Javascript/js/07_반복문.js
+++ 3_Javascript/js/07_반복문.js
@@ -81,11 +81,142 @@ function executeFn2() {
 // -> HTML 태그를 해석해서 화면에 태그의 본 기능대로 출력
 
 //------------------------------------------------------
+// 입력 받은 단 출력하기
 function executeFn3() {
+    // 입력된 값(단)
     const input = Number(document.getElementById("input3").value);
+
+    // 결과를 출력할 요소 ul태그
     const ul = document.getElementById("result3");
-    ul.innerHTML = "";
+
+    ul.innerHTML = ""; // 이전내용 삭제
+
+    // 입력 받은 단어 2~9 사이가 아닐 경우
+    // "2~9 사이만 입력해주세요" 알림창 띄우기
+
+    if (input < 2 || input > 9) {
+        alert("2~9 사이만 입력해 주세요");
+        return; // 함수를 종료하고 호출한 곳으로 돌아감
+        // 함수 종료 정도로만 인식 -> 밑에 코드 무시하고 executeFn3()을 벗어남
+    }
+
     for (let num = 1; num <= 9; num++) {
-        ul.innerHTML += `<li>${input} X ${num} = ${input * num}`;
+        ul.innerHTML += `<li> ${input} X ${num} = ${input * num} </li>`;
+    }
+}
+
+/* 다음 모양 출력하기
+    12345
+    12345
+    12345
+    12345
+    */
+
+function check8() {
+    for (let num = 1; num <= 4; num++) {
+        let str = "";
+        for (let num2 = 1; num2 <= 5; num2++) {
+            str += num2;
+        }
+        console.log(str);
+    }
+}
+
+function check9() {
+    let str = "";
+    for (let num2 = 1; num2 <= 5; num2++) {
+        str += num2;
+        console.log(str);
+    }
+}
+
+function check9() {
+    for (let num = 1; num <= 5; num++) {
+        let str = "";
+        for (let num2 = 1; num2 <= num; num2++) {
+            str += num2;
+        }
+        console.log(str);
+    }
+}
+
+// while 문 확인
+function check16() {
+    // 변수 선언만 해두기
+    let val; // undefined
+
+    // 취소를 누르기 전까지 반복
+    // == 취소를 누르면 반복하지 않겠다
+    while (val !== null) {
+        // 동일 비교 연산자
+        // !== 값, 자료형이 모두 다른 경우 true
+        // === 값, 자료형이 모두 같은 경우 true
+        val = prompt("입력 후 확인"); // 변수에 prompt 값 대입
+        // 확인 -> 입력한 값
+        // 취소 -> null
+
+        console.log(val);
+    }
+}
+
+function check17() {
+    // 메뉴 가격
+    const gimbap = 3000;
+    const ramen = 3500;
+    const donkaseu = 5000;
+
+    // 주문 개수 카운트
+    let gCount = 0; // 김밥
+    let rCount = 0; // 라면
+    let dCount = 0; // 돈까스
+
+    // prompt로 입력한 값을 저장할 변수 선언
+    let input; // undefined
+
+    while (input !== null) {
+        // 취소 누르기 전까지 반복
+
+        input = prompt("메뉴 번호를 입력하세요!");
+
+        switch (input) {
+            case "1":
+                gCount++;
+                break;
+            case "2":
+                rCount++;
+                break;
+            case "3":
+                dCount++;
+                break;
+            case null:
+                alert("주문 완료!");
+                break;
+            default:
+                alert("메뉴에 작성된 번호만 입력해주세요");
+                break;
+        }
+    }
+    alert(`김밥 : ${gCount}, 라면 : ${rCount}, 돈까스 : ${dCount}`);
+    let sum = gCount * gimbap + rCount * ramen + dCount * donkaseu;
+    alert(`총 가격 : ${sum} 원`);
+}
+
+// while을 for문처럼 사용하기
+function check18() {
+    // 1부터 10까지 출력
+
+    let num = 1;
+    while (num < 11) {
+        console.log(num);
+
+        num++;
+    }
+    console.log("---------------------------");
+
+    // 10부터 1까지 1씩 감소 (while)
+    let num2 = 10;
+    while (num2 > 0) {
+        console.log(num2);
+        num--;
     }
 }
---
+++ 3_Javascript/js/08_분기문.js
@@ -0,0 +1,71 @@
+// break 확인
+function check1() {
+    // 1부터 10까지 1씩 증가하다가 5가 되면 멈춤
+    for (let i = 1; i <= 10; i++) {
+        console.log("i : ", i);
+
+        if (i == 5) break;
+    }
+}
+
+// 무한 반복하는 while문 멈추기
+function check2() {
+    let i = 1;
+    // 10 초과 시 멈춤
+    while (true) {
+        // true 일 때 반복 수행하는 코드
+        console.log("i : ", i++);
+        if (i > 10) break;
+    }
+}
+
+// continue 확인하기
+function check3() {
+    // 1부터 20까지 출력, 단, 3의 배수는 건너뛰기
+    for (let i = 1; i <= 20; i++) {
+        // 3의 배수인 경우
+        if (i % 3 == 0) continue;
+
+        console.log("i : ", i);
+    }
+}
+/* 1부터 30까지 1씩 증가하며 출력
+    단, 홀수 또는 10의 배수는 건너뛰기
+    */
+function check4() {
+    for (let i = 1; i <= 30; i++) {
+        if (i % 2 == 1 || i % 10 == 0) continue;
+        console.log("i :", i);
+    }
+}
+
+/* 0~9까지 5줄 출력
+    - 각 줄에서 4의 배수는 건너뛰기
+    - 3번째 줄 출력 후 멈추기
+
+    0123456789
+    0123456789
+    0123456789
+*/
+function check5() {
+    for (let i = 1; i < 6; i++) {
+        let num = "0";
+        for (let x = 0; x < 10; x++) {
+            if (x % 4 == 0) continue;
+            num += x;
+        }
+        console.log(num);
+        if (i == 3) break;
+    }
+}
+
+function startGame() {
+    rand = Math.floor(Math.random() * 101);
+    input = prompt("1부터 100 사이 난수를 맞춰보세요");
+    if (rand == input) {
+        alert("맞히셨습니다");
+    }
+    if (input == null) {
+        alert("게임 취소");
+    }
+}
